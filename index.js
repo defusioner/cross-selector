d80ce35b678bc749697b7ebd101a0fb7a488af4d
Fnac <defusioner13@gmail.com>
2018-06-16 15:05:33 +0200

    FEAT - Remove pure html function version, support for multiple selectors at a time

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 const cheerio = require('cheerio');
 const request = require('request');
 
-const select = (url, selector) => {
+const select = (url, selectors, callback) => {
   return new Promise((resolve, reject) => {
     request(url, function (error, response, html) {
       if (!error && response.statusCode === 200) {
         const $ = cheerio.load(html);
-        const selectorResults = $(selector);
 
-        resolve(selectorResults.html())
-      } else {
-        reject(error);
-      }
-    });
-  });
-};
-
-const selectAll = (url, selector, callback) => {
-  return new Promise((resolve, reject) => {
-    request(url, function (error, response, html) {
-      if (!error && response.statusCode === 200) {
-        const $ = cheerio.load(html);
-        const items = $(selector);
+        const items = $(selectors.join(','));
 
         resolve(callback(cheerio, items))
       } else {
@@ -32,6 +18,5 @@ const selectAll = (url, selector, callback) => {
 };
 
 module.exports = {
-  select,
-  selectAll
+  select
 };
