72f97f71550b8142b1baac692770cc2082cec244
Fnac <defusioner13@gmail.com>
2018-06-23 09:27:31 +0200

    chore: Use arrow function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 const select = (url, selectors, callback) => {
   return new Promise((resolve, reject) => {
-    request(url, function (error, response, html) {
+    request(url, (error, response, html) => {
       if (!error && response.statusCode === 200) {
         const $ = cheerio.load(html);
 
