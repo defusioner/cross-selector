9a44dbb3d2980ae1b168fc9c8e14ef2cb8e79dd0
Fnac <defusioner13@gmail.com>
2018-06-23 09:27:16 +0200

    chore: Add playground test file

---
+++ test.js
@@ -0,0 +1,21 @@
+const select = require('.').select;
+
+const url = 'http://football.ua/';
+const selectors = [".main-news ul li a", ".main-article .text h3 a"];
+
+const callback = (cheerio, results) => {
+  let articles = [];
+  results.each((i, elem) => {
+    articles = [...articles, {
+      url: cheerio(elem).attr('href'),
+      text: cheerio(elem).text().trim()
+    }];
+  });
+  return articles;
+};
+
+select(url, selectors, callback).then(articles => {
+  console.log(articles)
+});
+
+
