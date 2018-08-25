d9a6ad4f2d1b1c8a85bed8f59d00a4dcba902648
Fnac <defusioner13@gmail.com>
2018-08-25 18:29:12 +0200

    chore: change one to one example to hackernews

--- examples/oneToOne.js
+++ examples/oneToOne.js
@@ -1,13 +1,17 @@
 const select = require('..').select;
 
-const url = "https://www.apple.com/shop/buy-mac/macbook-pro";
-const selectors = ".as-macbundle-modelspecs li";
+const url = 'https://news.ycombinator.com/';
+const selectors = '.athing .title > a';
 
 const callback = (cheerio, results) => {
-  if (cheerio(results).text().includes("8th-generation"))
-    return `Available!`;
-  else
-    return 'No new macs for today...';
+  let articles = [];
+  results.each((i, elem) => {
+    articles = [...articles, {
+      url: cheerio(elem).attr('href'),
+      text: cheerio(elem).text().trim()
+    }];
+  });
+  return articles;
 };
 
 select({url, selectors, callbacks: callback}).then(result => {
